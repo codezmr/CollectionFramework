e0b4a57a367ad88cd3d6754714bffc38725388d7
mdzam <mdzam@ZMR>
2022-04-09 10:39:22 +0530

    case1

---
+++ Collection_Interface_Methods/src/come/codezmr/methods/_04_removeAll.java
@@ -0,0 +1,27 @@
+package come.codezmr.methods;
+
+import java.util.ArrayList;
+
+public class _04_removeAll {
+
+	public static void main(String[] args) {
+		
+		ArrayList< String> al1 = new ArrayList<String>();
+		
+		al1.add("AAA");
+		al1.add("BBB");
+		al1.add("CCC");
+		al1.add("DDD");
+		al1.add("EEE");
+		System.out.println(al1);
+		
+		ArrayList<String> al2 = new ArrayList<String>();
+		al2.add("BBB");
+		al2.add("CCC");
+		System.out.println(al2);
+		System.out.println(al1.removeAll(al2));
+		System.out.println(al1);
+
+	}
+
+}
