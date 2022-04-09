40339bd174fcf44db28c4578c7cddeb62fad1f8e
mdzam <mdzam@ZMR>
2022-04-09 10:03:08 +0530

    Case2

--- Collection_Interface_Methods/src/come/codezmr/methods/AddMethod.java
+++ Collection_Interface_Methods/src/come/codezmr/methods/AddMethod.java
@@ -7,14 +7,33 @@ public class AddMethod {
 	public static void main(String[] args) {
 		
 		HashSet<String> hs = new HashSet<String>();
-		hs.add("ZMR");
-		hs.add("AAA");
-		hs.add("BBB");
-		hs.add("CCC");
-		hs.add("DDD");
+		System.out.println(hs.add("ZMR"));
+		System.out.println(hs.add("AAA"));
+		System.out.println(hs.add("CCC"));
+		System.out.println(hs.add("BBB"));
+		System.out.println(hs.add("DDD"));
+
 		System.out.println(hs);
 		
+		System.out.println(hs.add("CCC"));
+		System.out.println(hs.add("AAA"));
+		System.out.println(hs.add("KKK"));
+		System.out.println(hs);
 		
+		/*Output
+		  
+		    true
+			true
+			true
+			true
+			true
+			[AAA, CCC, BBB, DDD, ZMR]
+			false
+			false
+			true
+			[AAA, CCC, BBB, DDD, KKK, ZMR]
+			
+			*/
 	}
 
 }
