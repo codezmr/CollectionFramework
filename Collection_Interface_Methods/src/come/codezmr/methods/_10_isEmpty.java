962b569c1eb0b7e87122179c87f8a314e41d36b8
mdzam <mdzam@ZMR>
2022-04-09 19:14:29 +0530

    case2

--- Collection_Interface_Methods/src/come/codezmr/methods/_10_isEmpty.java
+++ Collection_Interface_Methods/src/come/codezmr/methods/_10_isEmpty.java
@@ -6,13 +6,10 @@ public class _10_isEmpty {
 
 	public static void main(String[] args) {
 		ArrayList<String> al1 = new ArrayList<String>();
-		al1.add("AAA");
-		al1.add("BBB");
-		al1.add("CCC");
-		al1.add("DDD");
+		
 		System.out.println(al1);
-		System.out.println(al1.isEmpty()); //false
-
+		System.out.println(al1.isEmpty()); //true
+		
 	}
 
 }
