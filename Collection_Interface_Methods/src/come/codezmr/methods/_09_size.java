831bd84554857c254b272b80a2f8b9aaf05cf767
mdzam <mdzam@ZMR>
2022-04-09 19:12:12 +0530

    case1

---
+++ Collection_Interface_Methods/src/come/codezmr/methods/_09_size.java
@@ -0,0 +1,19 @@
+package come.codezmr.methods;
+
+import java.util.ArrayList;
+
+public class _09_size {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> al = new ArrayList<String>();
+		al.add("AAA");
+		al.add("BBB");
+		al.add("CCC");
+		al.add("DDD");
+		System.out.println(al);
+		System.out.println(al.size());
+
+	}
+
+}
