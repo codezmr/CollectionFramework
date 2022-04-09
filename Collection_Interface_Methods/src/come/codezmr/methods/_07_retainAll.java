ce59b339989ec36a1b4ad09897b9cb424e025f9f
mdzam <mdzam@ZMR>
2022-04-09 18:59:05 +0530

    Case 2

--- Collection_Interface_Methods/src/come/codezmr/methods/_07_retainAll.java
+++ Collection_Interface_Methods/src/come/codezmr/methods/_07_retainAll.java
@@ -10,12 +10,13 @@ public class _07_retainAll {
 		al1.add("BBB");
 		al1.add("CCC");
 		al1.add("DDD");
-		al1.add("EEE");
 		System.out.println(al1);
 		
 		ArrayList<String> al2 = new ArrayList<String>();
+		al2.add("AAA");
 		al2.add("BBB");
 		al2.add("CCC");
+		al2.add("DDD");
 		System.out.println(al2);
 		System.out.println(al1.retainAll(al2));
 		System.out.println(al1);
