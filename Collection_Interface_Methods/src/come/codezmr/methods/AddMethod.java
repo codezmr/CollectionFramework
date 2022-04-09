45027c9d19d66f5cd2c9f3c88a4517dd9a28a6e7
mdzam <mdzam@ZMR>
2022-04-09 09:57:42 +0530

    Starting

---
+++ Collection_Interface_Methods/src/come/codezmr/methods/AddMethod.java
@@ -0,0 +1,20 @@
+package come.codezmr.methods;
+
+import java.util.HashSet;
+
+public class AddMethod {
+
+	public static void main(String[] args) {
+		
+		HashSet<String> hs = new HashSet<String>();
+		hs.add("ZMR");
+		hs.add("AAA");
+		hs.add("BBB");
+		hs.add("CCC");
+		hs.add("DDD");
+		System.out.println(hs);
+		
+		
+	}
+
+}
