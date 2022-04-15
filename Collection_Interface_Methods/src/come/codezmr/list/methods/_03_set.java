28ca1f00450ada5de27d0c2a804abee21c4b0e81
mdzam <mdzam@ZMR>
2022-04-16 04:52:55 +0530

    case1

---
+++ Collection_Interface_Methods/src/come/codezmr/list/methods/_03_set.java
@@ -0,0 +1,23 @@
+package come.codezmr.list.methods;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _03_set {
+
+	public static void main(String[] args) {
+		
+		
+		List<String> list = new ArrayList<String>();
+		list.add(0, "AAA");
+		list.add(1, "EEE");
+		list.add(2, "CCC");
+		list.add(3, "BBB");
+		list.add(4, "FFF");
+		System.out.println(list);//[AAA, EEE, CCC, BBB, FFF]
+		
+		list.set(1, "ZMR");
+		System.out.println(list);//[AAA, ZMR, CCC, BBB, FFF]
+	}
+
+}
