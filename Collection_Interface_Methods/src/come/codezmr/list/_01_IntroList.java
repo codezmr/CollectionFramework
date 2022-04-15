9217f402b5764ca52b8f3f7ddb10ee43959a2d05
mdzam <mdzam@ZMR>
2022-04-16 04:25:25 +0530

    case1

---
+++ Collection_Interface_Methods/src/come/codezmr/list/_01_IntroList.java
@@ -0,0 +1,10 @@
+package come.codezmr.list;
+
+public class _01_IntroList {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
---
+++ Collection_Interface_Methods/src/come/codezmr/list/methods/_01_add.java
@@ -0,0 +1,33 @@
+package come.codezmr.list.methods;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class _01_add {
+
+	public static void main(String[] args) {
+		
+		/*
+		 * It can be used to add the specified element at
+		 * the specified index.
+		 * 
+		 * If any element at the specified index then the provided
+		 * new element will be inserted at the specified index and
+		 * the existed element will be adjusted to the next index,this
+		 * operation is called as 'insert' opration.
+		 * 
+		 * 
+		 * 
+		 * */
+		
+		List<String> list = new ArrayList<String>();
+		list.add(0, "AAA");
+		list.add(1, "EEE");
+		list.add(2, "CCC");
+		list.add(3, "BBB");
+		list.add(4, "FFF");
+		System.out.println(list);
+		
+	}
+
+}
